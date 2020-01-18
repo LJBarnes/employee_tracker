51c8374464465703a8ee86aae76f8d2e158877d0
Leah Barnes <johnson.leahe@gmail.com>
2020-01-17 22:14:20 -0500

    updating tracker.js. view dept function not working.

--- tracker.js
+++ tracker.js
@@ -3,6 +3,9 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 var consoleTable = require("console.table");
 // var other file = functions etc ?
+var dept = [];
+var managers = [];
+var roleIds = [];
 
 // create the connection information for sql database
 var connection = mysql.createConnection({
@@ -18,11 +21,267 @@ var connection = mysql.createConnection({
 });
 
 // connect to mysql server and sql database
-connection.connect(function(err){
+connection.connect(function (err) {
     if (err) throw err;
     console.log("connection made");
     // run the start function after connection to prompt user
-    // start();
-    connection.end();
-    console.log("connection ended.");
+    start();
+    // connection.end();
+    // console.log("connection ended.");
 });
+
+// function for first prompt/asks user what action they want to take
+function start() {
+    inquirer
+        .prompt({
+            name: "action",
+            type: "list",
+            message: "What would you like to do?",
+            choices: ["Add Department", "Add Roles", "Add Employee", "View Departments", "View Roles", "View Employees", "Update Employee Role", "Exit"]
+        })
+        .then(function (answer) {
+            // based on answer, run appropriate function
+            if (answer.action === "Add Department") {
+                addDepartment();
+            }
+            else if (answer.action === "Add Roles") {
+                addRoles();
+            }
+            else if (answer.action === "Add Employee") {
+                addEmployee();
+            }
+            else if (answer.action === "View Department") {
+                viewDepartment();
+            }
+            else if (answer.action === "View Roles") {
+                viewRoles();
+            }
+            else if (answer.action === "View Employees") {
+                viewEmployees();
+            }
+            else if (answer.action === "Update EmployeeRole") {
+                updateRole();
+            }
+            else {
+                connection.end();
+            }
+        });
+}
+
+// function to handle adding a new department
+function addDepartment() {
+    inquirer
+        .prompt([
+            {
+                name: "addingdept",
+                type: "input",
+                message: "What is the name of the department you would like to add?"
+            }
+
+        ])
+        .then(function (answer) {
+            // when finished prompting, insert new department into db
+            connection.query(
+                "INSERT INTO department SET ?",
+                {
+                    name: answer.addingdept
+                },
+                function (err) {
+                    if (err) throw err;
+                    console.log("New department added successfully!");
+                    // re-prompt if they would like to do something else
+                    start();
+                }
+            );
+        });
+}
+// function for generating departments list
+function deptList() {
+    connection.query("SELECT id, dept_name FROM departments", function (err, res) {
+        if (err) throw err;
+        for (let i = 0; i < res.length; i++) {
+            dept.push(res[i].dept_name);
+        }
+    });
+}
+// function to handle adding a new department
+function addRoles() {
+    deptList();
+    inquirer
+        .prompt([
+            {
+                name: "title",
+                type: "input",
+                message: "What is the title of the role you're adding?"
+            },
+            {
+                name: "salary",
+                type: "input",
+                message: "What is the salary for the role you're adding?"
+            },
+            //    dont need a role id prompt-- role id is auto generated mysql
+            // =======================NOT WORKING============= it's not jiving with dept id...
+            // {
+            //     name: "deptname",
+            //     type: "list",
+            //     message: "What department is this role part of?",
+            //     choices: dept
+            // }
+
+        ])
+        .then(function (answer) {
+            // when finished prompting, insert new role with following info
+            connection.query(
+                // let deptname = 
+                "INSERT INTO roles SET ?",
+                {
+                    title: answer.title,
+                    salary: answer.salary,
+                    // department_id: answer.deptname
+                },
+                function (err) {
+                    if (err) throw err;
+                    console.log("New role added successfully!");
+                    // re-prompt if they would like to do something else
+                    start();
+                }
+            );
+        });
+}
+
+// function for generating role id list
+function roleIdList() {
+    connection.query("SELECT id, title FROM roles", function (err, res) {
+        if (err) throw err;
+        for (let i = 0; i < res.length; i++) {
+            roleIds.push(res[i].id, res[i].title);
+        }
+    });
+}
+// function for generating manager id list
+function managerIdList() {
+    connection.query("SELECT id, first_name, last_name FROM roles", function (err, res) {
+        if (err) throw err;
+        for (let i = 0; i < res.length; i++) {
+            roleIds.push(res[i].id, res[i].first_name, res[i].last_name);
+        }
+    });
+
+}
+
+function addEmployee() {
+    roleIdList();
+    // managerIdList(); -----breaks code------
+    inquirer
+        .prompt([
+            {
+                name: "firstname",
+                type: "input",
+                message: "What is the employee's first name?"
+            },
+            {
+                name: "lastname",
+                type: "input",
+                message: "What is the employee's last name?"
+            },
+            {
+                name: "roleid",
+                type: "input",
+                message: "Please enter their role id number."
+            },
+            {
+                name: "managerid",
+                type: "input",
+                message: "Please enter their manager's id number."
+            },
+            // =======DISPLAYS TITLE AND NUMBER CORRECTLY IN LIST BUT says id is undefined===
+            // {
+            //     name: "roleid",
+            //     type: "list",
+            //     message: "What is their role id number?",
+            //     choices: roleIds
+            // },
+            // {
+            //     name: "managerid",
+            //     type: "list",
+            //     message: "Who is their manager?",
+            //    choices: managers
+
+            // },
+
+
+        ])
+        .then(function (answer) {
+            // when finished prompting, insert new role with following info
+            connection.query(
+                "INSERT INTO employees SET ?",
+                {
+                    first_name: answer.firstname,
+                    last_name: answer.lastname,
+                    role_id: answer.roleid,
+                    manager_id: answer.managerid,
+                    // 
+                    // role_id: answer.roleids.id,
+                    // manager_id: answer.managerid.id
+                },
+                function (err) {
+                    if (err) throw err;
+                    console.log("New employee added successfully!");
+                    // re-prompt if they would like to do something else
+                    start();
+                }
+            );
+        });
+}
+// function to handle viewing departments
+// ======doesn't work==========no clue why=========
+function viewDepartment() {
+    connection.query("SELECT * FROM departments", function (err, res) {
+        if (err) throw err;
+        console.table(res)
+        start();
+    });
+};
+
+function viewRoles() {
+    connection.query("SELECT * FROM roles", function (err, res) {
+        if (err) throw err;
+        console.table(res)
+        start();
+    });
+};
+
+function viewEmployees() {
+    connection.query("SELECT * FROM employees", function (err, res) {
+        if (err) throw err;
+        console.table(res)
+        start();
+    });
+};
+
+
+
+
+    // function to handle viewing departments
+// function viewEmployees(){
+//     connection.query( "SELECT * FROM employees", function(err, res){
+//         if (err) throw err;
+//         console.log(`EMPLOYEES:`)
+//         res.forEach(employee => {
+//             console.log(`ID: ${employee.id} | Name: ${employee.first_name} ${employee.last_name} | Role ID: ${employee.role_id} | Manager ID: ${employee.manager_id}`);
+//         })
+//         start();
+//         });
+//     };    
+
+// function to handle viewing departments
+// function viewRoles(){
+//     connection.query( "SELECT * FROM roles", function(err, res){
+//         if (err) throw err;
+//         console.log(`EMPLOYEES:`)
+//         res.forEach(role => {
+//             console.log(`ID: ${role.id} | Name: ${role.title} ${role.salary} | Department ID: ${role.department_id}`);
+//         })
+//         start();
+//         });
+//     };  
--- trackerschema.sql
+++ trackerschema.sql
@@ -1,17 +1,50 @@
-DROP DATABASE IF EXISTS top_songsDB;
-CREATE DATABASE top_songsDB;
-USE top_songsDB;
+DROP DATABASE IF EXISTS employees_db;
 
-CREATE TABLE top5000 (
-    position INT NOT NULL,
-    artist_name VARCHAR (100),
-    song_name VARCHAR (100),
-    year INT,
-    raw_popularity DECIMAL(6,4),
-    usa_popularity DECIMAL(6,4),
-    uk_popularity DECIMAL(6,4),
-    europe_popularity DECIMAL(6,4),
-    world_popularity DECIMAL(6,4),
-    PRIMARY KEY (position)
+CREATE DATABASE employees_db;
 
+USE employees_db;
+
+CREATE TABLE departments (
+    id INT NOT NULL AUTO_INCREMENT,
+    dept_name VARCHAR(30) NOT NULL,
+    PRIMARY KEY(id)
 );
+
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(100) NOT NULL,
+    salary DECIMAL,
+    department_id INT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE employees (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NULL,
+    manager_id INT NULL,
+    PRIMARY KEY(id)
+);
+
+INSERT INTO departments(dept_name) VALUES ("Sales");
+INSERT INTO departments(dept_name) VALUES ("Marketing");
+INSERT INTO departments(dept_name) VALUES ("Finance");
+INSERT INTO departments(dept_name) VALUES ("Design");
+
+INSERT INTO roles ( title, salary, department_id)
+    VALUES ('Sales Lead', 50000, 1),
+    ('Sales Person', 40000, 1),
+    ('Lead Marketer', 100000, 2),
+    ('Marketing Assistant', 60000, 2),
+    ('Accountant', 120000, 3),
+    ('Design Director', 10000, 4),
+    ('Designer', 80000, 4);
+
+INSERT INTO employees ( first_name, last_name, role_id, manager_id)
+    VALUES 
+        ('Don', 'Maxx', 3, 1),
+        ('Maria','Sharp', 4, 3),
+        ('Alex', 'Jung', 1, NULL),
+        ('John', 'Doe', 1, 1);
+        
